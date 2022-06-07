3cbfebc2294fdd1c73ff40aae3f8ed5250211772
Dominguez Ortiz, Oscar <oscar.dominguez.ortiz@intel.com>
2022-06-07 15:35:27 -0700

    add more files

--- autobuild.sh
+++ autobuild.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 releasever="$1"
-./build.sh $releasever
+buildah unshare ./build.sh $releasever
 
+echo "Test image build Done!"
+
+echo "creating test container..."
+sleep 3
 testcontainer=$(buildah from "centos-stream-${releasever}-test")
+echo "test conianer created!"
 
+echo "testing RPMDB.."
 if buildah run $testcontainer -- /usr/lib/rpm/rpmdb_verify /var/lib/rpm/Packages | grep -q 'succeeded'; then
         echo "rpmdb is sane"
 else
